8e5c3dbc3af85416aaf5bd0a94d63c5f2d4bf495
SaintRepublic <saintrepublic@bk.ru>
2019-05-09 23:22:58 +0300

    version 1.0.6

--- framecontroller/src/main/java/com/saintrepublic/framecontroller/FCController.java
+++ framecontroller/src/main/java/com/saintrepublic/framecontroller/FCController.java
@@ -1,7 +1,7 @@
 package com.saintrepublic.framecontroller;
 
 /*
- * Copyright 2020 SaintRepublic
+ * Copyright 2019 SaintRepublic
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
